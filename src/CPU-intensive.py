d481d3c145ce6ca7c4d29972d87e40167613b405
Siddharth Mitra <s.mitra@intel.com>
2021-06-27 11:37:29 +0200

    Adding CPU intensive workload

---
+++ src/CPU-intensive.py
@@ -0,0 +1,20 @@
+import math
+
+
+def cpu_intensive_operation():
+    """
+    This function is supposed to mock a CPU intensive job
+    :return:
+    """
+    x = 0
+    for i in range(1000000000000):
+        x += math.sqrt(x)
+
+
+def main():
+    print("Starting the CPU intensive job")
+    cpu_intensive_operation()
+
+
+if __name__ == "__main__":
+    main()
