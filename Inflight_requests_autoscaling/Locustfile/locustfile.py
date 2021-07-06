33deaa3df417bc8b2f7637d05caf86173154548f
Siddharth Mitra <s.mitra@intel.com>
2021-07-06 17:35:36 +0200

    new changes

--- Inflight_requests_autoscaling/Locustfile/locustfile.py
+++ Inflight_requests_autoscaling/Locustfile/locustfile.py
@@ -10,15 +10,12 @@ class QuickstartUser(HttpUser):
 
     @task
     def hello_world(self):
-        url = "http://127.0.0.1:8080/v1/models/sklearn-iris:predict"
-        payload_t = "{\r\n  \"instances\": [\r\n    [6.8,  2.8,  4.8,  1.4],\r\n    [5.1,  3.5,  1.4,  0.2]\r\n  " \
-                    "]\r\n}\r\n "
         headers_t = {
-            'host': "sklearn-iris.kfserving-test.example.com",
+            'host': "torchscript-cifar10.test-service.example.com",
             'content-type': "application/json",
             'cache-control': "no-cache"
         }
-        with self.client.post("/v1/models/sklearn-iris:predict", data=payload_t, headers=headers_t,
+        with self.client.post("/v2/models/cifar10/infer", data=open('../input.json', 'rb'), headers=headers_t,
                               catch_response=True) as response:
             if response.status_code == 200:
                 response.success()
@@ -26,20 +23,3 @@ class QuickstartUser(HttpUser):
                 print(response.status_code)
                 response.failure("failure text")
 
-        @task
-        def hello_world(self):
-            url = "http://127.0.0.1:8080/v1/models/sklearn-iris:predict"
-            payload_t = "{\r\n  \"instances\": [\r\n    [6.8,  2.8,  4.8,  1.4],\r\n    [5.1,  3.5,  1.4,  0.2]\r\n  " \
-                        "]\r\n}\r\n "
-            headers_t = {
-                'host': "sklearn-iris.kfserving-test.example.com",
-                'content-type': "application/json",
-                'cache-control': "no-cache"
-            }
-            with self.client.post("/v1/models/sklearn-iris:predict", data=payload_t, headers=headers_t,
-                                  catch_response=True) as response:
-                if response.status_code == 200:
-                    response.success()
-                else:
-                    print(response.status_code)
-                    response.failure("failure text")
