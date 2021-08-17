7313f37dd9149874dcf81922ed054028213c2538
Siddharth Mitra <s.mitra@intel.com>
2021-08-17 21:44:22 +0200

    new

--- Locustfile/locustfile.py
+++ Locustfile/locustfile.py
@@ -6,17 +6,16 @@ from locust import HttpUser, task, between
 
 
 class QuickstartUser(HttpUser):
-    wait_time = between(1, 1.5)
 
     @task
     def hello_world(self):
         headers_t = {
-            'host': "flowers-sample-gpu.experiment-1.example.com",
+            'host': "flowers-sample-gpu.default.example.com",
             'content-type': "application/json",
             'cache-control': "no-cache"
         }
         with self.client.post("/v1/models/flowers-sample-gpu:predict", data=open(
-                '../InferenceServices/inflight-request-based/tensor-flow-flowers/input.json', 'rb'), headers=headers_t,
+                '../InferenceServices/GPU-based-hpa/input4.json', 'rb'), headers=headers_t,
                               catch_response=True) as response:
             if response.status_code == 200:
                 response.success()
